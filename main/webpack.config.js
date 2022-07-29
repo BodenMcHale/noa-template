68fcb1f324654639b2a9e3c835f1d23d2d49019f
Boden McHale <boden.mchale@gmail.com>
2022-07-28 20:09:40 -0400

    Initial Commit

---
+++ main/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+
+  // Output for the JavaScript bundle
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+
+  // Change the default size warnings
+  performance: {
+    maxEntrypointSize: 1.5e6,
+    maxAssetSize: 1.5e6,
+  },
+
+  devServer: {
+    static: {                               
+      directory: path.join(__dirname, './'),  
+      watch: true
+    },
+
+    // Automatically open the development server in a new tab
+    open: true,
+  },
+
+  // Gets rid of annoying Webpack stats
+  stats: "minimal",
+
+  // Make the development server polling use less CPU
+  watchOptions: {
+      aggregateTimeout: 500,
+      poll: 1000,
+      ignored: ["node_modules"],
+  },
+
+  plugins: [
+      new CleanWebpackPlugin(),  // Turning this on will remove files before the DevServer can access them, only use for builds
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'new_index.html'
+      })
+  ],
+};
